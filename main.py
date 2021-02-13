f2a22e3ceefdc8ce8c0cde55cb36233486e514ce
Fjanza <franciscojanza001@gmail.com>
2021-02-13 13:36:44 -0300

    Finalizacion del codigo con interfaz

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ def Qr():
     Cadena = input("Introduzca el texto para el codigo Qr: ")     #se ingresa el link que se abrira cuando se escanee el qr
     imagen = qrcode.make(Cadena)
 
-    nombre_imagen = input("Introduzca el nombre que tendra el archivo imagen: ")
-    archivo_imagen = open(nombre_imagen,'wb')
+    nombre_imagen = input("Introduzca el nombre que tendra el archivo imagen: ") + '.png'
+    path = 'Archivos_Qr/' + nombre_imagen
+    archivo_imagen = open(path,'wb')
     imagen.save(archivo_imagen)
     archivo_imagen.close()
 
@@ -17,7 +18,21 @@ def Qr():
 
 #-------------------------------DEFINICIÓN DEL MAIN--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 def main():
-    
+    condition = True
+
+    while condition:
+        Qr()
+        
+        opcion = input('Se desea crear mas codigos Qr Y/N: ')
+
+        if(opcion == 'Y'):
+            condition = True
+        
+        if(opcion == 'N'):
+            condition = False
+
+        if(opcion != 'Y' and opcion != 'N'):
+            condition = False    
 
 
 
