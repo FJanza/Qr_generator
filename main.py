23d61d6e49cacfa8b26a9072910b53151bcf945c
Fjanza <franciscojanza001@gmail.com>
2021-02-15 10:16:50 -0300

    implementacion de with en el codigo y arreglos de directorios

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
+import os
 import qrcode
 from PIL import Image
 
 
 #-------------------------------DEFINICIÓN DEL QRGenerator--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 def Qr():
-    Cadena = input("Introduzca el texto para el codigo Qr: ")     #se ingresa el link que se abrira cuando se escanee el qr
+    Cadena = input("Introduzca el texto para el codigo Qr: ")                                #se ingresa el link que se abrira cuando se escanee el qr
     imagen = qrcode.make(Cadena)
 
-    nombre_imagen = input("Introduzca el nombre que tendra el archivo imagen: ") + '.png'
+    nombre_imagen = input("Introduzca el nombre que tendra el archivo imagen: ") + '.png'    #se ingresa el nombre con el que se guardara el archivo
     path = 'Archivos_Qr/' + nombre_imagen
-    archivo_imagen = open(path,'wb')
-    imagen.save(archivo_imagen)
-    archivo_imagen.close()
+    with open(path,'wb') as archivo_imagen:
+        imagen.save(archivo_imagen)
+        
 
     return 0
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 #-------------------------------DEFINICIÓN DEL MAIN--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 def main():
+
+    if os.path.isdir('Archivos_Qr'):                                                          #pregunto si ya hay un directorio para guardar los Qr
+        pass
+
+    else:
+        os.mkdir('Archivos_Qr')                                                               #En el caso contrario creo dicho directorio
+
+
     condition = True
 
     while condition:
@@ -27,7 +36,7 @@ def main():
 
         if(opcion == 'Y'):
             condition = True
-        
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
         if(opcion == 'N'):
             condition = False
 
